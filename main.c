920c148e9710a30dcf168a2326d2cea382e5ca50
Thibaut FONTAINE <tfontain@e2r12p22.42.fr>
2017-03-26 15:47:53 +0200

    ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/26 10:04:27 by tfontain          #+#    #+#             */
-/*   Updated: 2017/03/26 15:30:34 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/03/26 15:43:45 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int				main()
 	p2 = malloc(sizeof(t_coord));
 	mlx->p = mlx_init();
 	mlx->w = mlx_new_window(mlx->p, 640, 480, "fdf");
-	p1->x = 500;
-	p1->y = 200;
-	p2->x = 100;
-	p2->y = 50;
+	p1->x = 50;
+	p1->y = 100;
+	p2->x = 200;
+	p2->y = 500;
 	print_line(*mlx, *p1, *p2, 0x000000FF);
 	mlx_loop(mlx->p);
 	return (0);
